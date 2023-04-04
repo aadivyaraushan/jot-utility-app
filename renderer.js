441608b59d6a58307a6a219915a5803539daaf93
Aadivya Raushan <96186374+aadivyaraushan@users.noreply.github.com>
2023-04-04 12:03:46 +0400

    adds dark mode

--- renderer.js
+++ renderer.js
@@ -1,3 +1,9 @@
 document.getElementById('closeButton').addEventListener('click', function () {
     window.close();
 });
+
+if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.getElementById('crossIcon').setAttribute('src', 'images/cross-dark-mode.png')
+} else {
+    document.getElementById('crossIcon').setAttribute('src', 'images/cross-light-mode.png')
+}
